92d2050829ac5e4cd5aab0562921169bbfc7485e
VemulaDhanush <Vemuladhanush300907@gmail.com>
2026-05-12 22:32:12 +0530

    updated backend url

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -5,7 +5,7 @@ function Dashboard({ onLogout }) {
   const [enquiries, setEnquiries] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/enquiry")
+   fetch("https://krishna-backend-41jq.onrender.com/api/enquiry")
       .then((res) => res.json())
       .then((data) => setEnquiries(data))
       .catch((err) => console.error("Fetch error:", err));
